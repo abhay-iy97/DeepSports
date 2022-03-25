21141352db292b19f7c0193e748821157cb1d937
Abhay Iyer <abhay.iyer6@gmail.com>
2022-03-24 18:34:25 -0700

    dataset generator script (#1)
    * dataset generator script
    
    * addressed review comments
    
    Co-authored-by: Abhay Iyer <adiyer@discovery2.hpc.usc.edu>

--- filter_frames/README.md
+++ filter_frames/README.md
@@ -1,14 +1,13 @@
 # How to Run
-1. Perform frame extraction on the videos using MTL-AQA's code [here](https://github.com/ParitoshParmar/MTL-AQA).
-2. Store the frames to **whole_videos_frames/XX** where XX is the video number (01, 02, .. 26)
-3. Copy the contents of this directory to be adjacent to whole_videos_frames, as shown below:
 
-- filter_frames.py
-- framesXX.txt
-- whole_videos_frames/
- 
-4. Run filter_frames.py from the command line using:
+Make changes in dataset_generator.sh
+1. Replace all "\<username\>" with your credentials. Example: adiyer
+2. Execute
+```
+sh dataset_generator.sh
+```
 
-```
-python3 filter_frames.py
-```
+### Result
+1. It will perform frame extraction on the videos using MTL-AQA's code [here](https://github.com/ParitoshParmar/MTL-AQA).
+2. Store the frames to ~/scratch1/\<username\>/deepsports_dataset/**whole_videos_frames/XX** where XX is the video number (01, 02, .. 26)
+3. Perform filtering of frames.
---
+++ filter_frames/download_videos.sh
@@ -0,0 +1,14 @@
+# #!/bin/bash
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AAAKpkawpb4mJJ4nhQmAPjxUa/01.mp4?dl=1 -L -o 01.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AADkp8iXHM4fgnzmy1jFwvKja/02.mp4?dl=1 -L -o 02.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AAARN1DMLxf8NABpSHKvZoZsa/03.mp4?dl=1 -L -o 03.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AABpMY694YU4XMJoWqg1JcYua/04.mp4?dl=1 -L -o 04.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AACTHGOs-kMzUVwVjxIv75pTa/07.mp4?dl=1 -L -o 07.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AAD3X5fCcQxsyiKlf6N_WDPva/09.mp4?dl=1 -L -o 09.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AAA_rOnzvnQ5rAWwhcRONjl9a/10.mp4?dl=1 -L -o 10.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AABT3mEX95cq9CHFJneV0wDCa/13.mp4?dl=1 -L -o 13.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AADDDD_g-dMONfAECq8Q5f9ma/14.mp4?dl=1 -L -o 14.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AAD0wDeC5SO_I62ndVWYqDn2a/17.mp4?dl=1 -L -o 17.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AAD-pVk90-ojr8TvNyrmo5iBa/18.mp4?dl=1 -L -o 18.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AADqXRHZpqLH-DlfKW3uNjb6a/22.mp4?dl=1 -L -o 22.mp4
+curl https://www.dropbox.com/sh/rt2eo97qt2bnrd4/AAC0z96ivZibxUGctjEPSqexa/26.mp4?dl=1 -L -o 26.mp4
